7a33f20609904a9b7fd24e065320ef7dd03130af
onokajames96 <jamesonoka996@gmail.com>
2023-05-17 23:26:28 -0700

    shell project

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function.
+ * @argc : argument count.
+ * @argv : argument vector.
+ *
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+char *prompt = ( "$");
+char *line;
+int j; 
+while(1)
+{
+printf("%s", prompt);
+}
+return (0);
+}
---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define MAX_INPUT_SIZE 1024
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#endif /* SHELL_H*/
