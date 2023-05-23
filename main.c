14e9b6ca282e0eb2bd3b478863f837acd6c73971
onokajames96 <jamesonoka996@gmail.com>
2023-05-23 06:34:51 -0700

    my shell

--- main.c
+++ main.c
@@ -11,12 +11,26 @@
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-char *prompt = ( "$");
-char *line;
-int j; 
+char *prompt = ("$");
+char *line = NULL;
+size_t buffsize = 0;
+ssize_t input_length;
+int i;
 while(1)
 {
 printf("%s", prompt);
+input_length = (getline(&line, &buffsize, stdin) == -1);
+if (feof(stdin))
+{
+free(line); /* avoid memory leaks when ctrl + d */
+exit(EXIT_SUCCESS);
+}
+else
+{
+free(line);
+perror("error while reading the line from stdin");
+exit(EXIT_FAILURE);
+}
 }
 return (0);
 }
