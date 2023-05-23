29b695269affda43e4c38f990bb4c8d8ae726023
onokajames96 <jamesonoka996@gmail.com>
2023-05-23 12:32:21 -0700

    my shell

--- shell.h
+++ shell.h
@@ -1,10 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 #define MAX_INPUT_SIZE 1024
+#define TOKEN_DELIM " \t\r\n\a"
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+#include<sys/stat.h>
+char *get_path(char *command);
+
 #endif /* SHELL_H*/
